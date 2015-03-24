c9d93971e1df8eafdb84a41d612c7928c77c64b5
Saleh Rastani <salehrastani@gmail.com>
2015-03-24 16:59:10 -0700

    api call sends single json object

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ weatherApp.config(function ($routeProvider){
 
 });
 
+
 weatherApp.service('cityService',function(){
   this.city = '';
-
 });
 
 
@@ -35,7 +35,17 @@ weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService
 
   $scope.city = cityService.city;
   $scope.weatherApi = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
-  $scope.weatherResult = $scope.weatherApi.get({q : $scope.city, cnt : 2});
+  $scope.weatherResult = $scope.weatherApi.get({q : $scope.city, cnt : 2}, function(data) {
+      $scope.days= data.list
+    });
 
+  $scope.convertTemperature = function(degK){
+    return Math.round((1.8 * (degK - 273)) + 32);
+  };
+
+  $scope.convertDate = function(dt){
+    return new Date(dt * 1000);
+  };
 
 }]);
+
